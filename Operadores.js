58872bebef8bfc1aad8868767644b4f32bc3b732
ulimbaMtz <102986728+ulimbaMtz@users.noreply.github.com>
2022-04-04 15:46:06 -0500

    Update Operadores.js

--- Operadores.js
+++ Operadores.js
@@ -50,3 +50,37 @@ function testLogicalAnd(val) {
 }
 
 testLogicalAnd(28);
+
+
+/*************************selecting-from-many-options-with-switch-statements***************/
+
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+
+switch(val){
+  case 1:
+    answer = "alpha";
+    break;
+  case 2:
+    answer = "beta";
+    break;
+  case 3:
+    answer = "gamma";
+    break;
+  case 4:
+    answer = "delta";
+    break;
+}
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(2);
+
+/**********use-the-conditional-ternary-operator*************/
+function checkEqual(a, b) {
+  return a === b ? "Equal" : "Not Equal";
+}
+
+checkEqual(1, 2);
