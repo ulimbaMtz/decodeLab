88f880895284edec007674647a28e1d0868b171c
ulimbaMtz <102986728+ulimbaMtz@users.noreply.github.com>
2022-04-04 11:59:31 -0500

    COD-1

---
+++ decodeVariables.js
@@ -0,0 +1,16 @@
+// Variable declarations
+//var StUdLyCapVaR;
+//var TitleCaseOver;
+
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+//STUDLYCAPVAR = 10;
+//PRoperCAmelCAse = "A String";
+//tITLEcASEoVER = 9000; 
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000; 
