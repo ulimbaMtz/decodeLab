38d3a54beeeea187e12b0931730572d9f7c9e574
ulimbaMtz <102986728+ulimbaMtz@users.noreply.github.com>
2022-04-04 20:47:11 -0500

    Create Arrays.js

---
+++ Arrays.js
@@ -0,0 +1,51 @@
+/*****************store-multiple-values-in-one-variable-using-javascript-arrays*****/
+
+// Only change code below this line
+const myArray = ["uno",1,"dos",2];
+
+
+/*************manipulate-arrays-with-push.txt*************/
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+myArray.push(["dog",3]);
+// Only change code below this line
+
+
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+var removedFromMyArray = myArray.pop();
+console.log(removedFromMyArray);
+// Only change code below this line
+
+/******************manipulate-arrays-with-pop*************/
+
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+var removedFromMyArray = myArray.pop();
+console.log(removedFromMyArray)
+// Only change code below this line
+
+// console output
+[ 'cat', 2 ]
+
+/**********************manipulate-arrays-with-shift****************/
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+
+var removedFromMyArray =  myArray.shift();
+console.log(removedFromMyArray);
+// Only change code below this line
+
+
+/**************manipulate-arrays-with-unshift.txt****************/
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+myArray.unshift(["Paul",35]);
+console.log(myArray);
+// Only change code below this line
+/* [ [ 'Paul', 35 ], [ 'dog', 3 ] ]/*
+
