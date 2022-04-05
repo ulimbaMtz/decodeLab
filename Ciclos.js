b4b4402f1105383b0d3574078ec7cc50b2bb63b5
ulimbaMtz <102986728+ulimbaMtz@users.noreply.github.com>
2022-04-04 19:17:34 -0500

    Create Ciclos.js

---
+++ Ciclos.js
@@ -0,0 +1,36 @@
+/************iterate-with-javascript-while-loops*****************/
+// Setup
+const myArray = [];
+let i = 5;
+
+while(i>=0){
+  myArray.push(i);
+  i--;
+}
+
+// Only change code below this line
+
+/************************iterate-with-javascript-for-loops************/
+// Setup
+const myArray = [];
+
+for (let i= 1; i<=5; i++){
+  myArray.push(i);
+}
+// Only change code below this line
+
+
+//*********************infite loop preventtion*****************/
+// Setup
+const myArray = [];
+let i = 1;
+
+// Only change code below this line
+do{
+  myArray.push(i);
+}while (i < 5);
+
+  Potential infinite loop detected on line 6. Tests may fail if this is not changed.
+
+  
+  
