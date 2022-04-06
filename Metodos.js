a573cb6e41788bfb1e1d20619e830a7e188e99f4
ulimbaMtz <102986728+ulimbaMtz@users.noreply.github.com>
2022-04-06 11:48:21 -0500

    Update Metodos.js

--- Metodos.js
+++ Metodos.js
@@ -21,3 +21,33 @@ return arr.join(" ");
 }
 
 sentensify("May-the-force-be-with-you");
+
+
+
+/**********************check-for-the-presence-of-an-element-with-indexof***************/
+
+function quickCheck(arr, elem) {
+  // Only change code below this line
+  var ret = false;
+if(arr.indexOf(elem)!=-1){
+ret = true;
+}
+return ret;
+  // Only change code above this line
+}
+
+console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+
+/********use-the-every-method-to-check-that-every-element-in-an-array-meets-a-criteria*************/
+function checkPositive(arr) {
+  // Only change code below this line
+  var ret = false;
+ret = arr.every(function(currentValue){
+      return currentValue > 0;
+});
+
+return ret;
+  // Only change code above this line
+}
+
+checkPositive([1, 2, 3, -4, 5]);
