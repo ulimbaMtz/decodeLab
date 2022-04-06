1e8e9bd94f51d3e9b3108e91b5f5a7ba06b47016
ulimbaMtz <102986728+ulimbaMtz@users.noreply.github.com>
2022-04-06 11:28:17 -0500

    Update Metodos.js

--- Metodos.js
+++ Metodos.js
@@ -1,1 +1,23 @@
 
+/*****************reverse-a-string********/
+function reverseString(str) {
+  var strReverse ="";
+  for(let i=str.length-1; i >= 0; i--){
+      strReverse += str[i];
+  }
+  return strReverse;
+}
+
+reverseString("hello");
+
+
+/********************combine-an-array-into-a-string-using-the-join-method*********************/
+
+function sentensify(str) {
+  // Only change code below this line
+const  arr = str.split(/\W/);
+return arr.join(" ");
+  // Only change code above this line
+}
+
+sentensify("May-the-force-be-with-you");
