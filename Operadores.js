865bda0ff64d022c63f7389d4d86e40f84f852f9
ulimbaMtz <102986728+ulimbaMtz@users.noreply.github.com>
2022-04-18 13:41:31 -0500

    Update Operadores.js

--- Operadores.js
+++ Operadores.js
@@ -15,26 +15,6 @@ const quotient = 4.4 / 2.0; // Change this line
 
 
 
-/******introducing-else-statements*********************/
-function testElse(val) {
-  let result = "";
-  // Only change code below this line
-
-  if (val > 5) {
-    result = "Bigger than 5";
-  }else{
-    result = "5 or Smaller";
-    }
-  
-  // Only change code above this line
-  return result;
-}
-
-testElse(4);
-
-
-
-
 /*************use-conditional-logic-with-if-statements  ***/
 
 function trueOrFalse(wasThatTrue) {
@@ -63,6 +43,85 @@ function testLogicalAnd(val) {
 }
 
 testLogicalAnd(28);
+
+
+/****************comparisons-with-the-logical-or-operator****************/
+/*function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val) {
+    return "Outside";
+  }
+
+  if (val) {
+    return "Outside";
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+testLogicalOr(15); */
+function testLogicalOr(val) {
+  // Only change code below this line
+  var ret ="";
+
+  if (val < 10 || val > 20) {
+    return "Outside";
+  }
+
+    // Only change code above this line
+  return "Inside";
+}
+
+testLogicalOr(15);
+
+/**************comparisons-with-the-logical-or-operato*******/
+/*function orderMyLogic(val) {
+  if (val < 10) {
+    return "Less than 10";
+  } else if (val < 5) {
+    return "Less than 5";
+  } else {
+    return "Greater than or equal to 10";
+  }
+} 
+
+orderMyLogic(7);*/
+
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+orderMyLogic(7);
+
+
+/******introducing-else-statements*********************/
+function testElse(val) {
+  let result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  }else{
+    result = "5 or Smaller";
+    }
+  
+  // Only change code above this line
+  return result;
+}
+
+testElse(4);
+
+
+
+
 
 
 /*************************selecting-from-many-options-with-switch-statements***************/
