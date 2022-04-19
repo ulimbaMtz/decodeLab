6415201ff643626c2f13f362983d4bcd1c44f8b7
ulimbaMtz <102986728+ulimbaMtz@users.noreply.github.com>
2022-04-18 22:04:16 -0500

    Update condicionales.js

--- condicionales.js
+++ condicionales.js
@@ -156,6 +156,62 @@ switch(val){
 
 caseInSwitch(2);
 
+
+
+/****************adding-a-default-option-in-switch-statements************/
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+  switch(val){
+    case "a":
+      answer = "apple";
+    break;
+    case "b":
+      answer = "bird";
+    break;
+    case "c":
+      answer = "cat";
+    break;
+    default:
+      answer = "stuff";
+    break;
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);
+
+
+/**************multiple-identical-options-in-switch-statements*******/
+function sequentialSizes(val) {
+  let answer = "";
+  // Only change code below this line
+switch(val){
+  case 1:
+  case 2:
+  case 3:
+    answer = "Low";
+    break;
+  case 4:  
+  case 5:
+  case 6:
+    answer = "Mid";
+    break;
+  case 7:  
+  case 8:
+  case 9:
+    answer = "High";
+    break;
+}
+  // Only change code above this line
+  return answer;
+}
+
+sequentialSizes(1);
+
 /**********use-the-conditional-ternary-operator*************/
 function checkEqual(a, b) {
   return a === b ? "Equal" : "Not Equal";
