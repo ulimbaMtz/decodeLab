feaef0b01d9cf8b047a919956055d201492f2146
ulimbaMtz <102986728+ulimbaMtz@users.noreply.github.com>
2022-04-19 08:40:49 -0500

    Update Ciclos.js

--- Ciclos.js
+++ Ciclos.js
@@ -20,7 +20,22 @@ for (let i= 1; i<=5; i++){
 // Only change code below this line
 
 
+/***********************prevent-infinite-loops-with-a-valid-terminal-condition*************/
+
+/*function myFunc() {
+  for (let i = 1; i != 4; i += 2) {
+    console.log("Still going!");
+  }
+}*/
+function myFunc() {
+  for (let i = 1; i <= 4; i += 2) {
+    console.log("Still going!");
+  }
+}
+
+
 //*********************infite loop preventtion*****************/
+
 // Setup
 const myArray = [];
 let i = 1;
@@ -31,6 +46,46 @@ do{
 }while (i < 5);
 
   Potential infinite loop detected on line 6. Tests may fail if this is not changed.
-
   
+  // Setup
+const myArray = [];
+let i = 1;
+
+// Only change code below this line
+do{
+  myArray.push(i);
+  i++; //previniendo loop
+}while (i < 5);
+
+
+// Only change code below this line
+
+/************************iterate-through-the-keys-of-an-object-with-a-for---in-statement (1)**************/
+/*CONTAR USUARIOS*/
+
+  const users = {
+  Alan: {
+    online: false
+  },
+  Jeff: {
+    online: true
+  },
+  Sarah: {
+    online: false
+  }
+}
+
+function countOnline(usersObj) {
+  // Only change code below this line
+  let contador = 0;
+    for(let usuarioObj in usersObj){
+      if(usersObj[usuarioObj].online){
+        contador +=1;
+      }  
+    }
+  return contador;
+  // Only change code above this line
+}
+
+console.log(countOnline(users));
   
