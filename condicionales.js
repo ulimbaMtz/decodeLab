cf1e4fab93892eb7882107c4e27c69c3c75b2cfb
ulimbaMtz <102986728+ulimbaMtz@users.noreply.github.com>
2022-04-18 19:52:35 -0500

    Update condicionales.js

--- condicionales.js
+++ condicionales.js
@@ -1,3 +1,30 @@
+/*****************use-conditional-logic-with-if-statements******************/
+function trueOrFalse(wasThatTrue) {
+  // Only change code below this line
+if(wasThatTrue){
+  return "Yes, that was true";
+ }else{
+   return "No, that was false";
+ }
+
+
+  // Only change code above this line
+
+}
+
+
+/**********************comparison-with-the-strict-equality-operator*****************/
+// Setup
+function testStrict(val) {
+  if (val === 7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testStrict(10);
+
+
 
 /******introducing-else-statements*********************/
 function testElse(val) {
@@ -18,7 +45,27 @@ testElse(4);
 
 
 
+/*********************chaining-if-else-statements***************************/
+function testSize(num) {
+  // Only change code below this line
+  var ret  = "";
+if(num<5){  
+  ret = "Tiny";
+}else if(num <10){
+  ret = "Small";
+}else if(num <15){
+  ret = "Medium";
+}else if(num <20){
+  ret = "Large";
+}else if(num>=20){
+  ret = "Huge";
+}
 
+  return ret;
+  // Only change code above this line
+}
+
+testSize(7);
 
 
 /*************************selecting-from-many-options-with-switch-statements***************/
