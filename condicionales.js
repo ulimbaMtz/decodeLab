1f59a4c08b247586994651195638b872e5eb9ceb
ulimbaMtz <102986728+ulimbaMtz@users.noreply.github.com>
2022-04-18 19:57:41 -0500

    Update condicionales.js

--- condicionales.js
+++ condicionales.js
@@ -44,6 +44,31 @@ function testElse(val) {
 testElse(4);
 
 
+/**************Logical Order in If Else Statements*******/
+/*function orderMyLogic(val) {
+  if (val < 10) {
+    return "Less than 10";
+  } else if (val < 5) {
+    return "Less than 5";
+  } else {
+    return "Greater than or equal to 10";
+  }
+} 
+
+orderMyLogic(7);*/
+
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+orderMyLogic(7);
+
 
 /*********************chaining-if-else-statements***************************/
 function testSize(num) {
